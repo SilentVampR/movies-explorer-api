2075d248e263c822339493f56b8fb715e6139d8a
SilentVampR <johnny.1983@yandex.ru>
2022-01-09 16:58:34 +0900

    test .env constants 3

--- controllers/users.js
+++ controllers/users.js
@@ -76,7 +76,7 @@ module.exports.getMe = (req, res, next) => {
       }
       return res
         .status(200)
-        .send({ data: user, env: NODE_ENV });
+        .send({ data: user, env: { production: NODE_ENV } });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
